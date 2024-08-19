0ee0357e382a5a547691cc30504f24cf342b7f92
rswade84 <renaldowade@gmail.com>
2024-08-19 14:15:51 -0500

    React Part1 Update Description

--- part1/studio/src/components/Description.jsx
+++ part1/studio/src/components/Description.jsx
@@ -1,15 +1,38 @@
+import styles from './description.module.css';
+import React from 'react';
+
+let recipeTitle = 'White Chocolate Blueberry Oatmeal Cookies';
+let recipeDescription =
+  'The most delicious, chewy blueberry oatmeal cookies with sweet white chocolate and coconut in every bite. These easy blueberry oatmeal cookies are made with coconut oil for double the coconut flavor and seriously taste like a fresh blueberry muffin. They’re my husband’s absolute favorite cookie!';
+
 const RecipeAuthor = () => {
-  let authorLink = ''; // url here
-  let authorPhoto = ''; // photo here
-  let authorName = ''; // name here
+  let authorLink =
+    'https://www.ambitiouskitchen.com/white-chocolate-blueberry-coconut-oatmeal-cookies/';
+  let authorPhoto =
+    'https://www.ambitiouskitchen.com/wp-content/uploads/2020/12/ak-mini-bio-2a-1-708x515.jpg';
+  let authorName = 'Monique Volz';
 
   return (
     <div className={styles.recipeAuthorBlock}>
       <img src={authorPhoto} alt={authorName} className={styles.imageUpdates} />
       <div>
         <h3>{authorName}</h3>
-        <a href={authorLink}>Blog name</a>
+        <a href={authorLink}>Ambitious Kitchen</a>
       </div>
     </div>
   );
 };
+
+class RecipeDescription extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>${recipeTitle}</h1>
+          <p>${recipeDescription}</p>
+        </div>
+        <RecipeAuthor />
+      </div>
+    );
+  }
+}
